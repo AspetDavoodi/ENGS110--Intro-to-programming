a90a1557928fddab79b6d9beab501eb6b9354788
AspetDavoodi <48726573+AspetDavoodi@users.noreply.github.com>
2019-03-19 23:39:35 +0400

    Update moneyCalculation_Aspet Davoodi.py

--- moneyCalculation_Aspet Davoodi.py
+++ moneyCalculation_Aspet Davoodi.py
@@ -1,7 +1,7 @@
 def calcCurrYearMoney (money, percent):
-    decimalPercent = percent/100
-    decimalPercent = decimalPercent + 1
-    money = money * decimalPercent
+    decimalPercent = percent/100 #turn given double digit percentage into decimal
+    decimalPercent = decimalPercent + 1 #add 1 to make the percentage more than 100%
+    money = money * decimalPercent #calculate change to input money
     return (money)
     
 
@@ -14,15 +14,15 @@ def main ():
     interestPercent = int ( input ("What is the interest rate percentage of your account? (integer number without % symbol- e.g. 50 or 10)"))
     yearsInBank = int ( input ("Integer between 1 and 20, number of years?"))
 
-    initValMoney = initMoney
-    initValYears = yearsInBank
+    initValMoney = initMoney #store the initial value for input money for later use (such that no changes are applied to it)
+    initValYears = yearsInBank #store the initial value for input years for later use (such that no changes are applied to it)
 
 
-    while yearsInBank > 0:
-        initMoney = calcCurrYearMoney (initMoney, interestPercent)
-        yearsInBank = yearsInBank - 1
+    while yearsInBank > 0: # main calculation
+        initMoney = calcCurrYearMoney (initMoney, interestPercent) #calculate new value for each year
+        yearsInBank = yearsInBank - 1 #for while loop to end
 
     
-    print ("After", initValYears, "years, your initial money", initValMoney, "has become, you final balance", initMoney)
+    print ("After", initValYears, "years, your initial money", initValMoney, "has become, you final balance", initMoney) #final prompt
 
 main ()
